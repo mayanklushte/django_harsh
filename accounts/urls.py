41fadc0ae2fc101a781e5a818129cfbc8c2ee40a
mayanklushte <lushtemayank21@gmil.com>
2021-06-29 10:01:47 +0530

    added login

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('user_register', views.user_register, name='user_register'),
+    path('user_login', views.user_login, name='user_login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import UserForm
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
 
 def index(request):
     name = 'mayank'
@@ -21,3 +24,23 @@ def user_register(request):
     else:
         form = UserForm()
     return render(request, 'user_register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username,password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse('user is not active')
+        else:
+            return HttpResponse('wrong credentials')
+    else:
+        return render(request, 'login.html', {})
